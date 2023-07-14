8f8691873b6473a37fce5f891a18aad13b1e09c0
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-07-14 10:09:39 +0300

    Adding admin and User

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,16 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import  User from './components/User';
+import Admin from './components/Admin';
 
 function App() {
-  const [carNumberPlate, setCarNumberPlate] = useState('');
-  const [ownerNames, setOwnerNames] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (carNumberPlate && ownerNames && phoneNumber) {
-      // Implement logic for processing the form data, such as sending it to the server
-      alert('Form submitted successfully!');
-    } else {
-      alert('Please fill in all the required fields.');
-    }
-  };
-
   return (
-    <div className="container">
-      <h1 className="my-4">Parking Application</h1>
-
-      <form onSubmit={handleFormSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Car Number Plate:</label>
-          <input
-            type="text"
-            className="form-control"
-            value={carNumberPlate}
-            onChange={(e) => setCarNumberPlate(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Owner's Official Names:</label>
-          <input
-            type="text"
-            className="form-control"
-            value={ownerNames}
-            onChange={(e) => setOwnerNames(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Phone Number:</label>
-          <input
-            type="text"
-            className="form-control"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={User} />
+        <Route exact path="/admin" component={Admin} />
+      </Switch>
+    </Router>
   );
 }
 
