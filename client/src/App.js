43f05a9ebfb0dc210b688bbcef2563988baa0c7e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-07-14 10:59:57 +0300

    Added the changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Navigation from './components/Navigation';
 import User from './components/User';
 import Admin from './components/Admin';
 
 function App() {
   return (
     <>
-      <Navigation />
 
       <div className="container my-4">
         <Routes>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import User from './User';
 
 function Home() {
   return (
     <div className="container">
-      <h1 className="my-4">Home Page</h1>
-      {/* Add content for the home page */}
+      <h1 className="my-4">Welcome to Crusade Event</h1>
+     <User />
     </div>
   );
 }
